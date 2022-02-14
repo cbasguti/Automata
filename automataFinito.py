d2bf611a4113c8d7f2561e0707c0e52dea006f56
cbasguti <byoxxill@gmail.com>
2022-02-13 22:54:34 -0500

    Commit inicial

--- automataFinito.py
+++ automataFinito.py
@@ -1,8 +1,5 @@
-from hashlib import new
-from sympy import true
-
-
 class AutomataFinito:
+    # Constructor
     def __init__(self):
         self.estados = []
         self.simbolos = []
@@ -10,47 +7,40 @@ class AutomataFinito:
         self.estadoFinal = []
         self.transiciones = {}
 
-    def setEstados(self, estados):
-        self.estados = estados
-
-    def setSimbolos(self, simbolos):
-        self.simbolos = simbolos
-
-    def setEstadoInicial(self, estadoInicial):
-        self.estadoInicial = estadoInicial
-
-    def setEstadoFinal(self, estadoFinal):
-        self.estadoFinal = estadoFinal
-
-    def setTransiciones(self, transiciones):
-        self.transiciones = transiciones
-
+    # Funcion que simplifica el automata
     def simplifica(self):
-        self.estadosExtraños()
+        self.estados_extraños()
         self.actualiza_estados()
-        self.estadosEquivalentes()
+        self.estados_equivalentes()
         self.actualiza_estados()
+        # No hay mucho que explicar :/
 
-    def estadosEquivalentes(self):
-        matriz = self.inicializa_matriz()
-        estados_equ = []
+    # Funcion que encuentra y unifica estados equivalentes
+    def estados_equivalentes(self):
+        matriz = self.inicializa_matriz() # matriz = matriz Nyhill-Nerode vacia
+        estados_equ = [] # lista vacia de estados equivalentes
+        # Ciclo que marca equivalencia = 1 cuando en un par de estados del automata
+        # uno es de aceptación y el otro no.
         for i in matriz:
             if i[0] in self.estadoFinal and i[1] not in self.estadoFinal:
                 i[2] = 1
             elif i[1] in self.estadoFinal and i[0] not in self.estadoFinal:
                 i[2] = 1
+        # Ciclo que marca equivalencia = 1 cuando las transiciones de un par de estados
+        # del automata fueron marcados con 1 anteriormente.
         for i in matriz:
             if i[2] == 0:
-                aux_1 = self.transiciones.get(i[0])
-                aux_2 = self.transiciones.get(i[1])
+                aux_1 = self.transiciones.get(i[0]) # transiciones estado 1
+                aux_2 = self.transiciones.get(i[1]) # transiciones estado 2
                 for k in range(len(self.simbolos)):
                     aux_3 = aux_1.get(self.simbolos[k])
                     aux_4 = aux_2.get(self.simbolos[k])
-                    if self.retornaEstado(aux_3, aux_4, matriz) == 1:
+                    if self.retorna_equivalencia(aux_3, aux_4, matriz) == 1:
                         i[2] = 1
+        # Ciclo que busca los pares de estados con equivalencia = 0, son los estados que 
+        # resultaron ser equivalentes siguiendo la teoría
         for i in matriz:
             if i[2] == 0:
-                #print(i)
                 aux_5 = []
                 aux_5.append(i[0])
                 aux_5.append(i[1])
@@ -58,18 +48,23 @@ class AutomataFinito:
                     estados_equ.append(aux_5)
                 else: 
                     for j in estados_equ:
+                        # Condicion que verifica si dos conjuntos de estados equivalentes deben ser 
+                        # unificados. ej:
+                        # [A,B] [C,B] -> [A,B,C]
                         if any(k in j for k in aux_5):
                             estados_equ[estados_equ.index(j)] += aux_5
                             estados_equ[estados_equ.index(j)] = list(dict.fromkeys(j))
                         else:
                             if estados_equ.count(aux_5) == 0:   
                                 estados_equ.append(aux_5)
-        self.unificaEstados(estados_equ)
+        # Unifica los conjuntos de estados equivalentes
+        self.unifica_estados(estados_equ)
 
-    def unificaEstados(self, lista_estados):
+    # Funcion que unifica los estados equivalentes
+    def unifica_estados(self, lista_estados):
         for estado in lista_estados:
-            tr_nuevas = {}
-            new_estado = ""
+            tr_nuevas = {} # transiciones del nuevo estado
+            new_estado = "" # estado nuevo
             # Almacena los nuevos estados
             for j in estado:
                     if self.estados.count(j) > 0:
@@ -92,7 +87,8 @@ class AutomataFinito:
                 if i in self.transiciones:
                     del self.transiciones[i]
                 
-    def retornaEstado(self, simbolo_a, simbolo_b, matriz):
+    # Funcion que retorna el estado de equivalencia de dos estados
+    def retorna_equivalencia(self, simbolo_a, simbolo_b, matriz):
         for i in matriz:
             if i[0] == simbolo_a and i[1] == simbolo_b:
                 return i[2]
@@ -102,7 +98,8 @@ class AutomataFinito:
                 return 0
         return "Error"
 
-    def estadosExtraños(self):
+    # Funcion que encuentra los estados extraños y los quita
+    def estados_extraños(self):
         tr_nuevas = []
         terminado = False
         estado_p = self.estadoInicial
@@ -130,6 +127,7 @@ class AutomataFinito:
                 self.estadoFinal.remove(i)
         self.estados = tr_nuevas
 
+    # Funcion que inicializa la matriz de Nyhill-Nerode
     def inicializa_matriz(self):
         # Matriz del Teorema de Myhill-Nerode
         # Esta matriz es útil para minimizar un autómata finito
@@ -144,6 +142,7 @@ class AutomataFinito:
                     matriz_mn.append(aux)
         return matriz_mn
 
+    # Funcion que convierte un AFND a un AFD
     def convierte_afnd(self):
         self.limpia_listas()
         terminado = False
@@ -180,6 +179,7 @@ class AutomataFinito:
         self.transiciones = tr_nuevas
         self.actualiza_estados()
 
+    # Funcion que hace que las transiciones '[C,D]' de un AFND sean 'CD'
     def limpia_listas(self):
         for i in self.transiciones:
             estado_actual = self.transiciones.get(i)
@@ -192,11 +192,13 @@ class AutomataFinito:
                     nuevo_estado = self.corrige_estado(nuevo_estado)
                     self.transiciones[i][j] = nuevo_estado
 
+    # Funcion que evita la existencia de estados anagramas o con caracteres repetidos
     def corrige_estado(self, estado):
         estado = sorted(estado)
         estado = "".join(dict.fromkeys(estado))
         return estado
      
+    # Funcion que actualiza los estados de aceptacion y el inicial
     def actualiza_estados(self):
         final_aux = self.estadoFinal.copy()
         for i in self.estados:
@@ -211,6 +213,22 @@ class AutomataFinito:
             if self.estadoInicial in i:
                 self.estadoInicial = i
         
+    # Setters del Automata
+
+    def set_estados(self, estados):
+        self.estados = estados
+
+    def set_simbolos(self, simbolos):
+        self.simbolos = simbolos
+
+    def set_inicial(self, estadoInicial):
+        self.estadoInicial = estadoInicial
+
+    def set_final(self, estadoFinal):
+        self.estadoFinal = estadoFinal
+
+    def set_transiciones(self, transiciones):
+        self.transiciones = transiciones
 
 
         
--- programa.py
+++ programa.py
@@ -1,34 +1,277 @@
-from automataFinito import AutomataFinito as AF
-from pythomata import SimpleDFA
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk, Image
-import graphviz as gv
-#import pydot
+from pythomata import SimpleDFA
+from automataFinito import AutomataFinito
+import sys, os
 
-a = AF()
-a.setEstados(['A', 'B', 'C'])
-a.setSimbolos(['0', '1'])
-a.setEstadoInicial('A')
-a.setEstadoFinal(['C'])
-transiciones = {
-    'A': {
-        '0': ['A', 'B'],'1': 'C'
-    },
-    'B': {
-        '0': 'A','1': ['B', 'C']
-    },
-    'C': {
-        '0': 'C','1': 'B'
-    }}
-a.setTransiciones(transiciones)
-a.convierte_afnd()
+config_name = 'myapp.cfg'
 
-print(set(a.estados))
-for i in a.transiciones:
-    print(i,' - ',a.transiciones.get(i))
+# determine if application is a script file or frozen exe
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
 
-b = SimpleDFA(set(a.estados), set(a.simbolos), a.estadoInicial, set(a.estadoFinal), a.transiciones)
-graph = b.to_graphviz()
-graph.render(directory='img') 
+config_path = os.path.join(application_path, config_name)
+
+lista_estados = []
+estado_inicial = ""
+lista_aceptacion = []
+lista_simbolos = []
+transiciones = {}
+entries = []
+checks = []
+auto_plugin = None
 
 
+
+menu_inicio = Tk()
+ventana_estados = Tk()
+ventana_e_inicial = Tk()
+ventana_e_final = Tk()
+
+## VENTANA PARA ESTADOS Y SIMBOLOS
+def comenzar():
+    menu_inicio.destroy()
+    estados_label_1.pack()
+    estados.pack()
+    estados_ing.pack()
+    simbolos_label_1.pack()
+    simbolos.pack()
+    simbolos_ing.pack()
+    boton_continuar_1.pack()
+    boton_mostrar.pack()
+    ventana_estados.eval('tk::PlaceWindow . center')
+    ventana_estados.mainloop()
+
+## VENTANA PARA ESTADO INICIAL Y ESTADOS DE ACEPTACION
+def continuar_1():
+    global lista_estados 
+    global lista_simbolos
+    global checks
+    lista_estados = list(estados.get().split(','))
+    lista_simbolos = list(simbolos.get().split(','))
+    ventana_estados.destroy()
+    e_inicial_label.pack()
+    variable.set(lista_estados[0])
+    e_inicial_menu = OptionMenu(ventana_e_inicial, variable, *lista_estados)  
+    e_inicial_menu.pack()
+    e_aceptacion_label.pack()
+    for estado in lista_estados:
+        new_estado = ttk.Checkbutton(ventana_e_inicial, text=estado)
+        new_estado.state(['!alternate'])
+        checks.append(new_estado)
+        new_estado.pack()
+    boton_continuar_2.pack()
+    ventana_e_inicial.eval('tk::PlaceWindow . center')
+    ventana_e_inicial.mainloop()
+
+## VENTANA CON LA TABLA DE TRANSICIONES
+def continuar_2():
+    global lista_estados
+    global lista_aceptacion
+    global lista_simbolos
+    global checks
+    global estado_inicial
+    for check in checks:
+        if 'selected' in str(check.state()):
+            lista_aceptacion.append(check.cget("text"))
+    estado_inicial = variable.get()
+    resumen_1 = Label(ventana_e_final, text="Los estados del automata son: " + str(lista_estados) +"\nLos simbolos de entrada son: " + str(lista_simbolos) + "\nEl estado inicial es: " + estado_inicial + "\nLos estados de aceptacion son: " + str(lista_aceptacion) + "\n\nAhora, por favor llene la tabla de transiciones usando los estados.\n(si desea ingresar un AFND, separe los estados por comas ','):\n", anchor="e", justify=LEFT)
+    resumen_1.grid(columnspan=len(lista_simbolos) + 2)
+    crea_tabla_tr(ventana_e_final)
+    boton_continuar_3.grid(column=1, columnspan=2)
+    ventana_e_inicial.destroy()
+    ventana_e_final.eval('tk::PlaceWindow . center')
+    ventana_e_final.mainloop()
+
+def continuar_3():
+    global transiciones
+    global lista_estados
+    global lista_simbolos
+    global entries
+    afnd = False
+    for tr in range(len(entries)):
+        tr_nuevas = {}
+        estado = lista_estados[tr]
+        obj = entries[tr]
+        for tk in range(len(obj)):
+            simbolo = lista_simbolos[tk]
+            if obj[tk].get().count(',') > 0:
+                tr_nuevas[simbolo] = list(obj[tk].get().split(','))
+                afnd = True
+            else:
+                tr_nuevas[simbolo] = obj[tk].get()
+        transiciones[estado] = tr_nuevas
+    label_estados.grid(columnspan=len(lista_simbolos) + 2)
+    label_final.grid(columnspan=len(lista_simbolos) + 2)
+    iniciar_programa(ventana_e_final, afnd)
+    ventana_e_final.eval('tk::PlaceWindow . center')
+    boton_continuar_3["state"] = "disabled"
+
+def iniciar_programa(root, afnd):
+    global lista_estados
+    global lista_aceptacion
+    global lista_simbolos
+    global estado_inicial
+    global transiciones
+    global auto_plugin
+    automata = AutomataFinito()
+    automata.set_estados(lista_estados)
+    automata.set_inicial(estado_inicial)
+    automata.set_final(lista_aceptacion)
+    automata.set_simbolos(lista_simbolos)
+    automata.set_transiciones(transiciones)
+    if afnd:
+        automata.convierte_afnd()
+    else:
+        automata.simplifica()
+    auto_plugin = SimpleDFA(
+        set(automata.estados), 
+        set(automata.simbolos), 
+        automata.estadoInicial, 
+        set(automata.estadoFinal), 
+        automata.transiciones)
+    muestra_resultado(root, automata)
+    fila = root.grid_size()
+    fila = fila[1] + 2
+    boton_muestra_d.grid(row=fila, column=1, columnspan=len(lista_simbolos))
+    label_secuencia.grid(row=fila+1, column=0, columnspan=len(lista_simbolos) + 2)
+    secuencia.grid(row=fila+2, column=0,columnspan=len(lista_simbolos) + 2)
+    label_acepta.grid(row=fila+3, column=1, columnspan=len(lista_simbolos))
+    boton_acepta_s.grid(row=fila+4, column=1, columnspan=len(lista_simbolos))
+    transiciones = {}
+
+def muestra_resultado(root, automata):
+    if label_estados.winfo_exists():
+        label_estados.config(text = "Los NUEVOS estados del automata son: " + str(automata.estados), anchor="e", justify=LEFT)
+    if label_final.winfo_exists():
+        label_final.config(text = "Los NUEVOS estados de aceptacion son: " + str(automata.estadoFinal) + "\nLas NUEVAS transiciones son:", anchor="e", justify=LEFT)
+    for row in range(len(automata.estados) + 1):
+        fila = label_final.grid_info()['row'] + 2
+        fila += row
+        for column in range(len(automata.simbolos) + 2):
+            if row > 0 and column > 0 and column < (len(automata.simbolos) + 1):
+                tr_resultado = Label(root, width=5, text=automata.transiciones.get(automata.estados[row - 1]).get(automata.simbolos[column - 1]))
+                tr_resultado.grid(row=fila, column=column)
+            if column == len(automata.simbolos) + 1:
+                if row != 0:
+                    if automata.estadoFinal.count(automata.estados[row - 1]) != 0:
+                        label_x = Label(root, text="1")
+                    else:
+                        label_x = Label(root, text="0")
+                    label_x.grid(row=fila, column=column)
+            elif row == 0 and column == 0:
+                c = True
+            else:
+                if row == 0:
+                    label_x = Label(root, text=automata.simbolos[column - 1])
+                    label_x.grid(row=fila, column=column)
+                elif column == 0:
+                    label_x = Label(root, text=automata.estados[row - 1])
+                    label_x.grid(row=fila, column=column)
+                
+def muestra_estados():
+    lista_estados = estados.get().split(',')
+    if estados_ing.winfo_exists():
+        estados_ing.config(text = lista_estados) 
+    lista_simbolos = simbolos.get().split(',')
+    if simbolos_ing.winfo_exists():
+        simbolos_ing.config(text = lista_simbolos) 
+    
+def muestra_diagrama():
+    global lista_simbolos
+    global auto_plugin
+    graph = auto_plugin.to_graphviz()
+    graph.render(directory='')     
+    graph.view()      
+
+def acepta_secuencia():
+    global auto_plugin
+    texto_secuencia = str(secuencia.get())
+    if auto_plugin.accepts(texto_secuencia):
+        label_acepta.config(text="Acepta secuencia")
+    else: 
+        label_acepta.config(text="Rechaza secuencia")
+
+def crea_tabla_tr(root):
+    global lista_estados
+    global lista_simbolos
+    global lista_aceptacion
+    global entries
+    for row in range(len(lista_estados) + 1):
+        cols = []
+        for column in range(len(lista_simbolos) + 2):
+            if column == len(lista_simbolos) + 1:
+                if row != 0:
+                    if lista_aceptacion.count(lista_estados[row - 1]) != 0:
+                        label_x = Label(root, text="1")
+                    else:
+                        label_x = Label(root, text="0")
+                    label_x.grid(row=row + 1, column=column)
+            elif row == 0 and column == 0:
+                c = True
+            else:
+                if row == 0:
+                    label_x = Label(root, text=lista_simbolos[column - 1])
+                    label_x.grid(row=row + 1, column=column)
+                elif column == 0:
+                    label_x = Label(root, text=lista_estados[row - 1])
+                    label_x.grid(row=row + 1, column=column)
+                else:
+                    tr_nueva = Entry(root, width=5)
+                    cols.append(tr_nueva)
+                    tr_nueva.grid(row=row + 1, column=column)
+        if cols:
+            entries.append(cols)
+
+
+
+estados_label_1 = Label(ventana_estados, text="Por favor, ingrese los estados de su automata sin espacios y separados por comas ',':")
+estados_ing = Label(ventana_estados, text = "Aqui se mostraran los estados ingresados")
+simbolos_label_1 = Label(ventana_estados, text="Por favor, ingrese los simbolos de su automata sin espacios y separados por comas ',':")
+simbolos_ing = Label(ventana_estados, text = "Aqui se mostraran los simbolos ingresados")
+e_inicial_label = Label(ventana_e_inicial, text="Por favor seleccion un estado inicial de los ingresados anteriormente:")
+e_aceptacion_label = Label(ventana_e_inicial, text="A continuacion, seleccione los estados de aceptacion:")
+label_estados = Label(ventana_e_final, text="")
+label_final = Label(ventana_e_final, text="")
+label_secuencia = Label(ventana_e_final, text="Ingrese una secuencia de Simbolos para verificar si es aceptada:")
+label_acepta = Label(ventana_e_final, text="No se ha ingresado secuencia")
+
+variable = StringVar(ventana_e_inicial)
+
+estados = Entry(ventana_estados, width=50)
+simbolos = Entry(ventana_estados, width=50)
+secuencia = Entry(ventana_e_final, width=50)
+
+boton_mostrar = Button(ventana_estados, text="MOSTRAR", command=muestra_estados)
+boton_continuar_1 = Button(ventana_estados, text="CONTINUAR", command=continuar_1)
+boton_continuar_2 = Button(ventana_e_inicial, text="CONTINUAR", command=continuar_2)
+boton_continuar_3 = Button(ventana_e_final, text="CONVERTIR / SIMPLIFICAR", command=continuar_3)
+boton_acepta_s = Button(ventana_e_final, text="INGRESAR", command=acepta_secuencia)
+boton_muestra_d = Button(ventana_e_final, text="MUESTRA DIAGRAMA", command=muestra_diagrama)
+
+ventana_estados.withdraw()
+ventana_e_inicial.withdraw()
+ventana_e_final.withdraw()
+
+
+
+# Importing a image
+img = ImageTk.PhotoImage(Image.open("Automata.png"))
+panel = Label(menu_inicio, image = img)
+panel.pack(side = "top", fill = "both", expand = "yes")
+
+# Creatinng a Label Widget
+myLabel1 = Label(menu_inicio, text="¡Bienvenido al programa Automata!")
+myLabel2 = Label(menu_inicio, text="Este programa le permite al usuario ingresar AFD y AFND para su posterior simplificación y verificación de secuencias")
+# Shoving it onto the screen
+myLabel1.pack()
+myLabel2.pack()
+# Boton para iniciar
+myButton = Button(menu_inicio, text="Comenzar", command=comenzar)
+myButton.pack()
+
+menu_inicio.eval('tk::PlaceWindow . center')
+menu_inicio.mainloop()
